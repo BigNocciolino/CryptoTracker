9d4bff5226002036152ea3a7c844b4213f7adcce
Pepegabruh <alfonsosorrentino211@gmail.com>
2021-04-14 10:08:31 +0200

    added more config

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
+[![GitHub Activity](https://img.shields.io/github/commit-activity/y/PepegaBruh/CryptoTrakcer?style=for-the-badge)](https://github.com/PepegaBruh/CryptoTrakcer/commits/main)
+[![hacs_badge](https://img.shields.io/badge/HACS-Default-orange.svg?style=for-the-badge)](https://github.com/custom-components/hacs)
+
 # CryptoTracker
 
-# This project is currently in testing
+This is an integration for Home Assistant to track over 300 cryptocurrencies.
 
-## Info
+## Insta
 
 ## Available options
 
--- custom_components/CryptoTrakcer/sensor.py
+++ custom_components/CryptoTrakcer/sensor.py
@@ -7,48 +7,56 @@ import logging
 from homeassistant.components.sensor import PLATFORM_SCHEMA, SensorEntity
 import homeassistant.helpers.config_validation as cv
 import voluptuous as vol
+#TODO Switch to Fiat and crypto
 from homeassistant.const import (
     CONF_NAME,
     STATE_UNKNOWN,
+    CONF_CURRENCY,
 )
 
 _LOGGER = logging.getLogger(__name__)
 
 ICON = "mdi:cash-multiple"
 
-SCAN_INTERVAL = timedelta(minutes=5)
+SCAN_INTERVAL = timedelta(seconds=30)
 
-ATTRIBUTION = "data provided by cryptonator api"
+ATTRIBUTION = "Data provided by cryptonator api"
+
+DEFAULT_FIAT = "EUR"
+
+DEFAULT_CURRENCY = "DOGE"
 
 DOMAIN = "cryptostate"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME): cv.string
-})
+CONF_ARG = "arg"
 
-# TODO add a config to get the request to do
-compareCurrency = ["eur", "usd"]
-needCompare = ["doge","btc","eth"]
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
+    vol.Optional(CONF_NAME, default=DOMAIN): cv.string,
+
+    vol.Required(CONF_CURRENCY, default=DEFAULT_CURRENCY): vol.All(
+        cv.ensure_list,
+        [
+            vol.Schema({
+                vol.Optional(CONF_ARG):cv.string
+            })
+        ]
+    )
+
+#    vol.schema ({
+#        vol.Required(CONF_CURRENCY, default=DEFAULT_CURRENCY): cv.string,
+#        vol.Optional(CONF_ARG, default=DEFAULT_FIAT): cv.string,
+#    })   
+})
 
 url = "https://api.cryptonator.com/api/ticker/{0}-{1}"
 
-def getRequest(toCompare, comapred):
-    """get The request from the api"""
-    parsedUrl = url.format(toCompare, comapred)
+def getData(crypto, fiat):
+    """Get The request from the api"""
+    parsedUrl = url.format(crypto, fiat)
     #The headers are used to simulate a human request
     req = requests.get(parsedUrl, headers={"User-Agent": "Mozilla/5.0 (Platform; Security; OS-or-CPU; Localization; rv:1.4) Gecko/20030624 Netscape/7.1 (ax)"}) 
 
-    return req
-
-def toJson():
-    """Transofrm the request into a json"""
-    #This is for TESTING ONLY
-    jsone = (getRequest("doge", "eur").json())
-    return jsone
-    
-def toPython():
-    """Tranform json in python library"""
-    jsone = toJson()
+    jsone = req.json()
     resp = json.dumps(jsone)
     respParsed = json.loads(resp)
 
@@ -57,16 +65,20 @@ def toPython():
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Setup the currency sensor"""
     name = config.get(CONF_NAME)
+    currency = config.get(CONF_CURRENCY)
+    fiat = config.get(CONF_ARG)
 
-    add_entities([CurrencySensor(hass, name)], True)
+    add_entities([CurrencySensor(hass, name, currency, fiat)], True)
 
 class CurrencySensor(SensorEntity):
     
-    def __init__(self, hass, name):
+    def __init__(self, hass, name, currency, fiat):
         """Inizialize sensor"""
         self._state = STATE_UNKNOWN
         self._name = name
         self._hass = hass
+        self._currency = currency
+        self._fiat = fiat
 
     @property
     def name(self):
@@ -91,4 +103,4 @@ class CurrencySensor(SensorEntity):
     def update(self):
         """Get the latest update fron the api"""
 
-        self._state = toPython() 
+        self._state = getData(self._currency, self.fiat)
