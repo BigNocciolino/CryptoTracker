1081aad5818b8fbade8cba8895421d56e43ad68b
Pepegabruh <alfonsosorrentino211@gmail.com>
2021-04-15 12:04:22 +0200

    added resourced

--- custom_components/CryptoTrakcer/sensor.py
+++ custom_components/CryptoTrakcer/sensor.py
@@ -11,6 +11,8 @@ from homeassistant.const import (
     CONF_NAME,
     STATE_UNKNOWN,
     CONF_CURRENCY,
+    ATTRIBUTION,
+    CONF_RESOURCES,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -30,9 +32,16 @@ DOMAIN = "cryptostate"
 CONF_FIAT = "fiat"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_CURRENCY, default=DEFAULT_CURRENCY): cv.string,
-    vol.Required(CONF_FIAT, default=DEFAULT_FIAT): cv.string,   
-    vol.Optional(CONF_NAME, default=DOMAIN): cv.string,
+    vol.Required(CONF_RESOURCES, default=[]): vol.All(
+        cv.ensure_list,
+        [
+            vol.Schema({
+                vol.Required(CONF_CURRENCY, default=DEFAULT_CURRENCY): cv.string,
+                vol.Optional(CONF_NAME, default=DOMAIN): cv.string,
+                vol.Optional(CONF_FIAT, default=DEFAULT_FIAT): cv.string,
+            })
+        ],
+    ),
 })
 
 url = "https://api.cryptonator.com/api/ticker/{0}-{1}"
@@ -47,15 +56,25 @@ def getData(crypto, fiat):
     resp = json.dumps(jsone)
     respParsed = json.loads(resp)
 
-    return respParsed["ticker"]["price"]
+    if (respParsed["success"] == True):
+        return respParsed["ticker"]["price"]
+    else:
+        _LOGGER.warn("Request unsuccessful")
+        _LOGGER.error(respParsed["error"])
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Setup the currency sensor"""
-    name = config.get(CONF_NAME)
-    currency = config.get(CONF_CURRENCY)
-    fiat = config.get(CONF_FIAT)
 
-    add_entities([CurrencySensor(hass, name, fiat, currency)], True)
+    entities = []
+
+    for resource in config[CONF_RESOURCES]:
+        fiat = resource[CONF_FIAT]
+        currency = resource[CONF_CURRENCY]
+        name = resource[CONF_NAME]
+        
+        entities.append(CurrencySensor(hass, name, fiat, currency))
+
+    add_entities(entities, True)
 
 class CurrencySensor(SensorEntity):
     
@@ -80,12 +99,16 @@ class CurrencySensor(SensorEntity):
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement of this entity, if any."""
-        return "EUR"
+        return self._fiat
 
     @property
     def state(self):
         """Return the state of the sensor"""
         return self._state
+
+    @property
+    def device_state_attributes(self):
+        return ATTRIBUTION
 
     def update(self):
         """Get the latest update fron the api"""
