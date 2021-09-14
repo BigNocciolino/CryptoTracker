33b27a86305b777034f69642a486fff5eefa504e
PepegaBruh <alfonsosorrentino211@gmail.com>
2021-09-14 16:05:43 +0200

    code clean

---
+++ custom_components/CryptoTrakcer/const.py
@@ -0,0 +1,16 @@
+"""Const used by CryptoTracker"""
+from __future__ import annotations
+
+from datetime import timedelta
+
+ICON = "mdi:cash-multiple"
+
+SCAN_INTERVAL = timedelta(seconds=60)
+
+ATTRIBUTION = "Data provided by cryptonator api"
+
+DEFAULT_COMPARE = "doge-eur"
+
+CONF_COMPARE = "compare"
+
+DOMAIN = "cryptostate"
