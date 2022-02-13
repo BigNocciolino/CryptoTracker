7a7136e3616f724a740b1a34517ab7c353121598
BigNocciolino <alfonsosorrentino211@gmail.com>
2022-02-13 11:51:58 +0100

    remove hardcoded sensor parameters

--- custom_components/cryptostate/sensor.py
+++ custom_components/cryptostate/sensor.py
@@ -1,7 +1,7 @@
 """Sensor platform for integration_blueprint."""
 from homeassistant.components.sensor import SensorEntity
 
-from .const import DOMAIN, ICON
+from .const import DOMAIN, ICON, CONF_BASE
 from homeassistant.const import CONF_NAME
 from .entity import CryptoTrackerEntity
 
@@ -10,15 +10,17 @@ async def async_setup_entry(hass, entry, async_add_devices):
     """Setup sensor platform."""
     coordinator = hass.data[DOMAIN][entry.entry_id]
     name = entry.data.get(CONF_NAME)
-    async_add_devices([CryptoTrackerSensor(coordinator, entry, name)])
+    base = entry.data.get(CONF_BASE)
+    async_add_devices([CryptoTrackerSensor(coordinator, entry, name, base)])
 
 
 class CryptoTrackerSensor(CryptoTrackerEntity, SensorEntity):
     """integration_blueprint Sensor class."""
 
-    def __init__(self, coordinator, config_entry, name):
+    def __init__(self, coordinator, config_entry, name, base):
         super().__init__(coordinator, config_entry)
         self._name = name
+        self._base = base
 
     @property
     def name(self):
@@ -27,9 +29,8 @@ class CryptoTrackerSensor(CryptoTrackerEntity, SensorEntity):
 
     @property
     def native_value(self):
-        """Return the native value of the sensor."""
-        # TODO redifine default value,
-        return self.coordinator.data.get("eur")
+        """Return the native value of the sensor.""",
+        return self.coordinator.data.get(self._base)
 
     @property
     def icon(self):
