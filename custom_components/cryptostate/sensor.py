2c55d50d3f9cb7334de6c698384a98bb289e74c2
BigNocciolino <alfonsosorrentino211@gmail.com>
2022-02-16 18:02:10 +0100

    New commit for fix conflict

---
+++ custom_components/cryptostate/sensor.py
@@ -0,0 +1,38 @@
+"""Sensor platform for integration_blueprint."""
+from homeassistant.components.sensor import SensorEntity
+
+from .const import DOMAIN, ICON, CONF_BASE
+from homeassistant.const import CONF_NAME
+from .entity import CryptoTrackerEntity
+
+
+async def async_setup_entry(hass, entry, async_add_devices):
+    """Setup sensor platform."""
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+    name = entry.data.get(CONF_NAME)
+    base = entry.data.get(CONF_BASE)
+    async_add_devices([CryptoTrackerSensor(coordinator, entry, name, base)])
+
+
+class CryptoTrackerSensor(CryptoTrackerEntity, SensorEntity):
+    """integration_blueprint Sensor class."""
+
+    def __init__(self, coordinator, config_entry, name, base):
+        super().__init__(coordinator, config_entry)
+        self._name = name
+        self._base = base
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return f"{self._name}"
+
+    @property
+    def native_value(self):
+        """Return the native value of the sensor.""",
+        return self.coordinator.data.get(self._base)
+
+    @property
+    def icon(self):
+        """Return the icon of the sensor."""
+        return ICON
